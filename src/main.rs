ac123129c6ab19a467834050da5f5d0e6991b627
EnoticCoder <samaks.sasha@gmail.com>
2026-05-15 14:49:42 +0300

    add 3d cube

--- src/main.rs
+++ src/main.rs
@@ -1,96 +1,101 @@
 use bevy::prelude::*;
-use bevy::window::WindowResolution;
 
 fn main() {
     App::new()
-        // Этот метод меняет фильтрацию по умолчанию с Linear на Nearest
-        .add_plugins(
-            DefaultPlugins
-                .set(WindowPlugin {
-                    primary_window: Some(Window {
-                        resolution: WindowResolution::new(800, 600),
-                        title: "Little Secret".to_string(),
-                        resizable: true,
-                        ..default()
-                    }),
-                    ..default()
-                })
-                .set(ImagePlugin::default_nearest())
-        )
+        .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
-        .add_systems(Update, controls)
+        .add_systems(Update, (rotate_cube, camera_controls))
         .run();
 }
 
 #[derive(Component)]
-struct Player;
+struct RotatingCube;
 
 #[derive(Component)]
-struct Box;
+struct MainCamera;
 
-fn controls(
-    mut player_query: Query<&mut Transform, With<Player>>,
-    input: Res<ButtonInput<KeyCode>>,
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) { 
+    //cube
+    commands.spawn((
+        Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
+        MeshMaterial3d(materials.add(StandardMaterial {
+            base_color: Color::srgb(0.8, 0.2, 0.3),
+            ..default()
+        })),
+        Transform::from_xyz(0.0, 0.0, 0.0),
+        RotatingCube,
+    ));
+    
+    //light
+    commands.spawn((
+        DirectionalLight {
+            illuminance: 1000.0,
+            ..default()
+        },
+        Transform::from_xyz(3.0, 5.0, 3.0).looking_at(Vec3::ZERO, Vec3::Y),
+    ));
+    
+    // Камера для 3D
+    commands.spawn((
+        MainCamera,
+        Camera3d::default(),
+        Transform::from_xyz(0.0, 0.0, 5.0),
+    ));
+}
+
+fn rotate_cube(
     time: Res<Time>,
-){
-    let mut transform = match player_query.single_mut() {
+    mut query: Query<&mut Transform, With<RotatingCube>>,
+) {
+    // for mut transform in query.iter_mut() {
+    //     transform.rotate_y(1.0 * time.delta_secs());
+    //     transform.rotate_x(0.5 * time.delta_secs());
+    // }
+}
+
+fn camera_controls(
+    time: Res<Time>,
+    keyboard: Res<ButtonInput<KeyCode>>,
+    mut query: Query<&mut Transform, With<MainCamera>>,
+) {
+    let mut transform = match query.single_mut() {
         Ok(t) => t,
         Err(_) => return,
     };
-
+    
     let mut direction = Vec3::ZERO;
+    let speed = 5.0;
 
-    if input.pressed(KeyCode::KeyW) {
-        direction.y += 1.0;
+    let forward = transform.forward().as_vec3();
+    let right = transform.right().as_vec3();
+    let up = transform.up().as_vec3();
+    
+    if keyboard.pressed(KeyCode::KeyW) {
+        direction += forward;
     }
-    if input.pressed(KeyCode::KeyS) {
-        direction.y -= 1.0;
+    if keyboard.pressed(KeyCode::KeyS) {
+        direction -= forward;
     }
-    if input.pressed(KeyCode::KeyD) {
-        direction.x += 1.0;
+    if keyboard.pressed(KeyCode::KeyA) {
+        direction -= right;
     }
-    if input.pressed(KeyCode::KeyA) {
-        direction.x -= 1.0;
+    if keyboard.pressed(KeyCode::KeyD) {
+        direction += right;
+    }
+    if keyboard.pressed(KeyCode::KeyQ) {
+        direction -= up;
+    }
+    if keyboard.pressed(KeyCode::KeyE) {
+        direction += up;
     }
     
     if direction != Vec3::ZERO {
         direction = direction.normalize();
+        transform.translation += direction * speed * time.delta_secs();
     }
-    
-    let speed = 300.0;
-    transform.translation += direction * speed * time.delta_secs();
-}
 
-fn setup(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>
-) {
-    commands.spawn(Camera2d::default());
-
-    let texture_handle = asset_server.load("player.png");
-    let box_texture = asset_server.load("box.png");
-
-    commands.spawn((
-        Player,
-        Sprite {
-            color: Color::srgb(1.0, 1.0, 1.0),
-            custom_size: Some(Vec2::new(100.0, 100.0)),
-            image: texture_handle,
-            ..default()
-        },
-        Transform::from_xyz(0.0, 0.0, 0.0),
-        Visibility::default(),
-    ));
-
-    commands.spawn((
-        Box,
-        Sprite {
-            color: Color::srgb(1.0, 1.0, 1.0),
-            custom_size: Some(Vec2::new(100.0, 100.0)),
-            image: box_texture,
-            ..default()
-        },
-        Transform::from_xyz(100.0, 0.0, 11.0),
-        Visibility::default(),
-    ));
 }

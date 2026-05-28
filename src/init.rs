8d13497ded08dfda73fae9136928dbc0df2a37db
EnoticCoder <samaks.sasha@gmail.com>
2026-05-28 06:19:29 +0300

    add init.rs

---
+++ src/init.rs
@@ -0,0 +1,260 @@
+use winit::window::Window;
+use wgpu::*;
+
+use crate::init_buffers;
+use crate::Vertex;
+use crate::DepthBuffer;
+
+#[allow(dead_code)]
+pub struct WgpuApp {
+    pub instance: wgpu::Instance,
+    pub device: wgpu::Device,
+    pub queue: wgpu::Queue,
+    pub surface_format: wgpu::TextureFormat,
+
+    pub uniform_buffer: wgpu::Buffer,
+    pub depth_stencil: wgpu::DepthStencilState,
+    pub transparent_depth_stencil: wgpu::DepthStencilState,
+    pub bind_group_layout: wgpu::BindGroupLayout,
+    pub bind_group: wgpu::BindGroup,
+    pub texture_bind_group_layout: wgpu::BindGroupLayout,
+    pub depth_buffer: DepthBuffer,
+
+    pub render_pipeline: wgpu::RenderPipeline,
+    pub transparent_pipeline: wgpu::RenderPipeline,
+    pub config: wgpu::SurfaceConfiguration,
+}
+
+impl WgpuApp{
+    pub async fn new(
+        window: &Window,
+    ) -> Self{
+        //instance (экземпляр)
+        //with defalt settings
+        let instance = wgpu::Instance::new(InstanceDescriptor::default());
+
+        //поверхность
+        let surface = instance.create_surface(window)
+            .expect("Failed to create surface");
+
+        //addapter/physical_device
+
+        //опции выбора видеокарты
+        let addapter_option = wgpu::RequestAdapterOptions {
+            //выбирает адаптер, совместимый с этой поверхностью (обычно дискретная видеокарта)
+            compatible_surface : Some(&surface),
+            //всё остальное поумолчанию
+            ..Default::default()
+        };
+
+        //запрашиваем физ устройство
+        let addapter_future = instance.request_adapter(&addapter_option);
+        //ожидаем
+        let addapter = pollster::block_on(addapter_future).unwrap();
+
+        println!("{}",addapter.get_info().name);
+        
+        //Log device
+        let (device, queue) = addapter
+        .request_device(
+            &DeviceDescriptor { //настройки устройства
+                //какие плагины необходимы
+                required_features: Features::empty(),
+                //минимальные требования
+                required_limits: Limits::default(),
+                //Отладка
+                label: None,
+            },
+            None,
+        )
+        .await
+        .unwrap();
+
+        //init buffers
+
+        let translation = [0.0, 0.0, 0.0, 0.0];
+        let rotation = [0.0, 0.0, 0.0, 0.0];
+        let window_size = window.inner_size();
+    
+        let buffers = init_buffers(
+            window_size,
+            translation,
+            rotation,
+            &device,
+        );
+
+        let uniform_buffer = buffers.uniform_buffer;
+        let depth_stencil = buffers.depth_stencil;
+        let depth_buffer = buffers.depth_buffer;
+        let transparent_depth_stencil = buffers.transparent_depth_stencil;
+        let bind_group_layout = buffers.bind_group_layout;
+        let bind_group = buffers.bind_groupprojection;
+        let texture_bind_group_layout = buffers.texture_bind_group_layout;
+
+        //shaders
+        //получаем код шейдера
+        let shader_code = include_str!(".././src/shaders.wgsl");
+        //shader object
+        //описание шейдера
+        let description = wgpu::ShaderModuleDescriptor {
+            //отладка
+            label : None,
+            //.into() - преобразует &str в Cow<'_, str> <- (владеть или читать)
+            source : wgpu::ShaderSource::Wgsl(shader_code.into()),
+        };
+        //Компилирует шейдер для GPU
+        let shader_module = device.create_shader_module(description);
+
+
+        //Render pipeline
+        //PipelineLayout
+        let pipeline_layout = device.create_pipeline_layout(&PipelineLayoutDescriptor {
+            label: Some("Pipeline Layout"),
+            bind_group_layouts: &[&bind_group_layout, &texture_bind_group_layout],
+            push_constant_ranges: &[], // константы, которые можно быстро обновлять
+        });
+        let caps = surface.get_capabilities(&addapter);
+        let surface_format = caps.formats[0];
+
+
+        let description = wgpu::RenderPipelineDescriptor {
+            label : Some("Render Pipeline"),
+            vertex : wgpu::VertexState {
+                buffers: &[wgpu::VertexBufferLayout {
+                    array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
+                    step_mode: wgpu::VertexStepMode::Vertex,
+                    attributes: &[
+                        wgpu::VertexAttribute {
+                            offset: 0,// смещение на 0 байт
+                            shader_location: 0,  // @location(0) в шейдере
+                            format: wgpu::VertexFormat::Float32x3,  // position: [f32; 2]
+                        },
+                        wgpu::VertexAttribute {
+                            offset: std::mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,// смещение 8 байт
+                            shader_location: 1,  // @location(1) в шейдере
+                            format: wgpu::VertexFormat::Float32x2,
+                        }
+                    ]
+                }],
+                module : &shader_module,
+                entry_point : "vs_main",
+            },
+
+            fragment : Some(wgpu::FragmentState {
+                targets: &[Some(ColorTargetState {
+                    format: surface_format,
+                    blend: Some(wgpu::BlendState {
+                        color: wgpu::BlendComponent {
+                            src_factor: wgpu::BlendFactor::SrcAlpha,   // источник = альфа
+                            dst_factor: wgpu::BlendFactor::OneMinusSrcAlpha, // фон = 1-альфа
+                            operation: wgpu::BlendOperation::Add,
+                        },
+                        alpha: wgpu::BlendComponent {
+                            src_factor: wgpu::BlendFactor::One,
+                            dst_factor: wgpu::BlendFactor::OneMinusSrcAlpha,
+                            operation: wgpu::BlendOperation::Add,
+                        },
+                    }),
+                    write_mask: ColorWrites::ALL,
+                })],
+                module : &shader_module,
+                entry_point : "fs_main",
+            }),
+            
+            primitive: PrimitiveState {
+                topology: PrimitiveTopology::TriangleList, // список треугольников
+                strip_index_format: None, // не используем полоски
+                front_face: FrontFace::Ccw, // против часовой стрелки = лицевая
+                cull_mode: None, // не отсекаем грани
+                unclipped_depth: false,
+                polygon_mode: PolygonMode::Fill, // заливаем цветом (не каркасный режим)
+                conservative: false, // не использовать консервативный растеризатор
+            },
+
+            layout : Some(&pipeline_layout),
+            depth_stencil: Some(depth_stencil.clone()),
+            multisample : Default::default(),
+            multiview : None,
+        };
+
+        let render_pipeline = device.create_render_pipeline(&description);
+
+        let transparent_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some("Transparent Pipeline"),
+            layout: Some(&pipeline_layout),
+            vertex: wgpu::VertexState {
+                module: &shader_module,
+                entry_point: "vs_main",
+                buffers: &[wgpu::VertexBufferLayout {
+                    array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
+                    step_mode: wgpu::VertexStepMode::Vertex,
+                    attributes: &[
+                        wgpu::VertexAttribute {
+                            offset: 0,
+                            shader_location: 0,
+                            format: wgpu::VertexFormat::Float32x3,
+                        },
+                        wgpu::VertexAttribute {
+                            offset: std::mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
+                            shader_location: 1,
+                            format: wgpu::VertexFormat::Float32x2,
+                        }
+                    ]
+                }],
+            },
+            fragment: Some(wgpu::FragmentState {
+                module: &shader_module,
+                entry_point: "fs_main",
+                targets: &[Some(wgpu::ColorTargetState {
+                    format: surface_format,
+                    blend: Some(wgpu::BlendState::ALPHA_BLENDING),  // прозрачные - со смешиванием
+                    write_mask: wgpu::ColorWrites::ALL,
+                })],
+            }),
+            primitive: wgpu::PrimitiveState {
+                topology: wgpu::PrimitiveTopology::TriangleList,
+                strip_index_format: None,
+                front_face: wgpu::FrontFace::Ccw,
+                cull_mode: None,
+                unclipped_depth: false,
+                polygon_mode: wgpu::PolygonMode::Fill,
+                conservative: false,
+            },
+            depth_stencil: Some(transparent_depth_stencil.clone()),  // ← используем transparent
+            multisample: wgpu::MultisampleState::default(),
+            multiview: None,
+        });
+
+        let config = SurfaceConfiguration {
+            usage: TextureUsages::RENDER_ATTACHMENT, // использовать как цель рендеринга
+            format: surface_format, // формат пикселей (BGRA8)
+            width: window_size.width, // ширина окна
+            height: window_size.height, // высота окна
+            present_mode: PresentMode::Fifo, //вертикальная синхронизация
+            alpha_mode: CompositeAlphaMode::Auto, // альфа-канал автоматический
+            view_formats: vec![], // дополнительные форматы для текстур
+            desired_maximum_frame_latency: 2, // задержка кадров (2 = баланс)
+        };
+
+        surface.configure(&device, &config);
+        
+        Self{
+            instance,
+            device,
+            queue,
+            surface_format,
+
+            uniform_buffer,
+            depth_stencil,
+            transparent_depth_stencil,
+            bind_group_layout,
+            bind_group,
+            texture_bind_group_layout,
+            depth_buffer,
+
+            render_pipeline,
+            transparent_pipeline,
+            config,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ use winit::{
     event_loop::{EventLoop},
     window::WindowBuilder,
 };
-use wgpu::*;
 use winit::dpi::PhysicalSize;
 use tokio;
 
@@ -17,6 +16,7 @@ mod ui_panels;
 mod sprite_manager;
 mod slot_object;
 mod input;
+mod init;
 
 use egui_manager::EguiManager;
 use ui_panels::UiState;
@@ -27,263 +27,61 @@ use render::*;
 use sprite_manager::*;
 use slot_object::*;
 use input::*;
+use init::*;
 
 #[tokio::main]
 async fn main() {
-    //основной цикл winit
+
     let event_loop = EventLoop::new().unwrap();
+
     //winit window
     let window = WindowBuilder::new()
         .with_title("game")
         .with_inner_size(PhysicalSize::new(800, 800))
         .build(&event_loop)
         .unwrap();
-    
-    //instance (экземпляр)
-    //with defalt settings
-    let instance = wgpu::Instance::new(InstanceDescriptor::default());
+
+    let mut wgpu_app = WgpuApp::new(&window).await;
 
     //поверхность
-    let surface = instance.create_surface(&window)
+    let surface = wgpu_app.instance.create_surface(&window)
         .expect("Failed to create surface");
 
-    //addapter/physical_device
+    //sprites
 
-    //опции выбора видеокарты
-    let addapter_option = wgpu::RequestAdapterOptions {
-        //выбирает адаптер, совместимый с этой поверхностью (обычно дискретная видеокарта)
-        compatible_surface : Some(&surface),
-        //всё остальное поумолчанию
-        ..Default::default()
-    };
-
-    //запрашиваем физ устройство
-    let addapter_future = instance.request_adapter(&addapter_option);
-    //ожидаем
-    let addapter = pollster::block_on(addapter_future).unwrap();
-
-    println!("{}",addapter.get_info().name);
-    
-    //Log device
-    let (device, queue) = addapter
-    .request_device(
-        &DeviceDescriptor { //настройки устройства
-            //какие плагины необходимы
-            required_features: Features::empty(),
-            //минимальные требования
-            required_limits: Limits::default(),
-            //Отладка
-            label: None,
-        },
-        None,
-    )
-    .await
-    .unwrap();
-
-    //init buffers
-
-    let translation = [0.0, 0.0, 0.0, 0.0];
-    let rotation = [0.0, 0.0, 0.0, 0.0];
-    let window_size = window.inner_size();
- 
-    let mut buffers = init_buffers(
-        window_size,
-        translation,
-        rotation,
-        &device,
-    );
-
-    let uniform_buffer = &buffers.uniform_buffer;
-    let depth_stencil = buffers.depth_stencil;
-    let transparent_depth_stencil = buffers.transparent_depth_stencil;
-    let bind_group_layout = &buffers.bind_group_layout;
-    let bind_group = &buffers.bind_groupprojection;
-    let texture_bind_group_layout = &buffers.texture_bind_group_layout;
-
-    // Создаём блок
-
-    let mut cursor: Sprite = Sprite::new(&device, &queue, "./tex/cursor/def_cursor.png", [0,0], [1,1]);
+    let mut cursor: Sprite = Sprite::new(&wgpu_app.device, &wgpu_app.queue, 
+        "./tex/cursor/def_cursor.png", [0,0], [1,1]);
     cursor.translation = [4.0,4.0,0.0,1.0];
-    cursor.build_buffers(&device);
+    cursor.build_buffers(&wgpu_app.device);
 
     let mut map:Vec<Sprite> = Vec::new();
     let decor:Vec<Sprite> = Vec::new();
 
     for i in 0..10{
         for j in 0..10{
-            let mut block: Sprite = Sprite::new(&device, &queue, "tex/floor.png", [0,0], [2,2]);
+            let mut block: Sprite = Sprite::new(&wgpu_app.device, &wgpu_app.queue, "tex/floor.png", [0,0], [2,2]);
             block.translation = [i as f32 - 4.0, j as f32 - 4.0, 0.0, 1.0];
-            block.build_buffers(&device);
+            block.build_buffers(&wgpu_app.device);
 
             map.push(block);
         }
     }
 
-    let mut game : GameObjects = GameObjects {cursor, map, decor, groups: Vec::new(),};
-
-    //shaders
-    //получаем код шейдера
-    let shader_code = include_str!(".././src/shaders.wgsl");
-    //shader object
-    //описание шейдера
-    let description = wgpu::ShaderModuleDescriptor {
-        //отладка
-        label : None,
-        //.into() - преобразует &str в Cow<'_, str> <- (владеть или читать)
-        source : wgpu::ShaderSource::Wgsl(shader_code.into()),
-    };
-    //Компилирует шейдер для GPU
-    let shader_module = device.create_shader_module(description);
-
-
-    //Render pipeline
-    //PipelineLayout
-    let pipeline_layout = device.create_pipeline_layout(&PipelineLayoutDescriptor {
-        label: Some("Pipeline Layout"),
-        bind_group_layouts: &[&bind_group_layout, &texture_bind_group_layout],
-        push_constant_ranges: &[], // константы, которые можно быстро обновлять
-    });
-    let caps = surface.get_capabilities(&addapter);
-    let surface_format = caps.formats[0];
-
-
-    let description = wgpu::RenderPipelineDescriptor {
-        label : Some("Render Pipeline"),
-        vertex : wgpu::VertexState {
-            buffers: &[wgpu::VertexBufferLayout {
-                array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
-                step_mode: wgpu::VertexStepMode::Vertex,
-                attributes: &[
-                    wgpu::VertexAttribute {
-                        offset: 0,// смещение на 0 байт
-                        shader_location: 0,  // @location(0) в шейдере
-                        format: wgpu::VertexFormat::Float32x3,  // position: [f32; 2]
-                    },
-                    wgpu::VertexAttribute {
-                        offset: std::mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,// смещение 8 байт
-                        shader_location: 1,  // @location(1) в шейдере
-                        format: wgpu::VertexFormat::Float32x2,
-                    }
-                ]
-            }],
-            module : &shader_module,
-            entry_point : "vs_main",
-        },
-
-        fragment : Some(wgpu::FragmentState {
-            targets: &[Some(ColorTargetState {
-                format: surface_format,
-                blend: Some(wgpu::BlendState {
-                    color: wgpu::BlendComponent {
-                        src_factor: wgpu::BlendFactor::SrcAlpha,   // источник = альфа
-                        dst_factor: wgpu::BlendFactor::OneMinusSrcAlpha, // фон = 1-альфа
-                        operation: wgpu::BlendOperation::Add,
-                    },
-                    alpha: wgpu::BlendComponent {
-                        src_factor: wgpu::BlendFactor::One,
-                        dst_factor: wgpu::BlendFactor::OneMinusSrcAlpha,
-                        operation: wgpu::BlendOperation::Add,
-                    },
-                }),
-                write_mask: ColorWrites::ALL,
-            })],
-            module : &shader_module,
-            entry_point : "fs_main",
-        }),
-        
-        primitive: PrimitiveState {
-            topology: PrimitiveTopology::TriangleList, // список треугольников
-            strip_index_format: None, // не используем полоски
-            front_face: FrontFace::Ccw, // против часовой стрелки = лицевая
-            cull_mode: None, // не отсекаем грани
-            unclipped_depth: false,
-            polygon_mode: PolygonMode::Fill, // заливаем цветом (не каркасный режим)
-            conservative: false, // не использовать консервативный растеризатор
-        },
-
-        layout : Some(&pipeline_layout),
-        depth_stencil: Some(depth_stencil),
-        multisample : Default::default(),
-        multiview : None,
-    };
-
-    let render_pipeline = device.create_render_pipeline(&description);
-
-    let transparent_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-        label: Some("Transparent Pipeline"),
-        layout: Some(&pipeline_layout),
-        vertex: wgpu::VertexState {
-            module: &shader_module,
-            entry_point: "vs_main",
-            buffers: &[wgpu::VertexBufferLayout {
-                array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
-                step_mode: wgpu::VertexStepMode::Vertex,
-                attributes: &[
-                    wgpu::VertexAttribute {
-                        offset: 0,
-                        shader_location: 0,
-                        format: wgpu::VertexFormat::Float32x3,
-                    },
-                    wgpu::VertexAttribute {
-                        offset: std::mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
-                        shader_location: 1,
-                        format: wgpu::VertexFormat::Float32x2,
-                    }
-                ]
-            }],
-        },
-        fragment: Some(wgpu::FragmentState {
-            module: &shader_module,
-            entry_point: "fs_main",
-            targets: &[Some(wgpu::ColorTargetState {
-                format: surface_format,
-                blend: Some(wgpu::BlendState::ALPHA_BLENDING),  // прозрачные - со смешиванием
-                write_mask: wgpu::ColorWrites::ALL,
-            })],
-        }),
-        primitive: wgpu::PrimitiveState {
-            topology: wgpu::PrimitiveTopology::TriangleList,
-            strip_index_format: None,
-            front_face: wgpu::FrontFace::Ccw,
-            cull_mode: None,
-            unclipped_depth: false,
-            polygon_mode: wgpu::PolygonMode::Fill,
-            conservative: false,
-        },
-        depth_stencil: Some(transparent_depth_stencil),  // ← используем transparent
-        multisample: wgpu::MultisampleState::default(),
-        multiview: None,
-    });
-
-    let mut config = SurfaceConfiguration {
-        usage: TextureUsages::RENDER_ATTACHMENT, // использовать как цель рендеринга
-        format: surface_format, // формат пикселей (BGRA8)
-        width: window_size.width, // ширина окна
-        height: window_size.height, // высота окна
-        present_mode: PresentMode::Fifo, //вертикальная синхронизация
-        alpha_mode: CompositeAlphaMode::Auto, // альфа-канал автоматический
-        view_formats: vec![], // дополнительные форматы для текстур
-        desired_maximum_frame_latency: 2, // задержка кадров (2 = баланс)
-    };
-
-    surface.configure(&device, &config);
-
+    let mut game : GameObjects = GameObjects {cursor, map, decor, groups: Vec::new()};
 
     //main loop vars
     let mut input = WinitInputHelper::new();
 
-    //0 - Standart
-    //1 - build
-    //2 - Break
-
     let mut mode = 0;
+
+    //slots
 
     let mut act_slot:i32 = 0;
     let mut slots:Vec<Slot> = vec![
         Slot{
             id: 1,
             obj: Object{
-                sprite: Sprite::new(&device, &queue, "tex/decor/table.png", [1,0], [2,1]),
+                sprite: Sprite::new(&wgpu_app.device, &wgpu_app.queue, "tex/decor/table.png", [1,0], [2,1]),
                 width: 2, height: 1, name: String::from("table"),
             },
             active: true,
@@ -291,7 +89,7 @@ async fn main() {
         Slot{
             id: 2,
             obj: Object{
-                sprite: Sprite::new(&device, &queue, "tex/decor/carpet.png", [0,0], [2,2]),
+                sprite: Sprite::new(&wgpu_app.device, &wgpu_app.queue, "tex/decor/carpet.png", [0,0], [2,2]),
                 width: 1, height: 1, name: String::from("carpet"),
             },
             active: false,
@@ -299,7 +97,7 @@ async fn main() {
         Slot{
             id: 3,
             obj: Object{
-                sprite: Sprite::new(&device, &queue, "tex/decor/sign.png", [0,0], [1,1]),
+                sprite: Sprite::new(&wgpu_app.device, &wgpu_app.queue, "tex/decor/sign.png", [0,0], [1,1]),
                 width: 1, height: 1, name: String::from("lamp"),
             },
             active: false,
@@ -308,8 +106,8 @@ async fn main() {
 
     //INIT UV
     let mut egui_manager = EguiManager::new(
-        &device,
-        surface_format,
+        &wgpu_app.device,
+        wgpu_app.surface_format,
         None,  // depth format
         1,     // samples
         &window,
@@ -321,7 +119,8 @@ async fn main() {
             Slot {
                 id: 1,
                 obj: Object {
-                    sprite: Sprite::new(&device, &queue, "tex/decor/table.png", [0,0], [2,1]),
+                    sprite: Sprite::new(&wgpu_app.device, &wgpu_app.queue, 
+                        "tex/decor/table.png", [0,0], [2,1]),
                     width: 2, height: 1, name: String::from("table"),
                 },
                 active: true,
@@ -329,7 +128,7 @@ async fn main() {
             Slot {
                 id: 2,
                 obj: Object {
-                    sprite: Sprite::new(&device, &queue, "tex/decor/carpet.png", [0,0], [2,2]),
+                    sprite: Sprite::new(&wgpu_app.device, &wgpu_app.queue, "tex/decor/carpet.png", [0,0], [2,2]),
                     width: 1, height: 1, name: String::from("carpet"),
                 },
                 active: false,
@@ -337,7 +136,7 @@ async fn main() {
             Slot{
                 id: 3,
                 obj: Object{
-                    sprite: Sprite::new(&device, &queue, "tex/decor/sign.png", [0,0], [1,1]),
+                    sprite: Sprite::new(&wgpu_app.device, &wgpu_app.queue, "tex/decor/sign.png", [0,0], [1,1]),
                     width: 1, height: 1, name: String::from("lamp"),
                 },
                 active: false,
@@ -357,7 +156,7 @@ async fn main() {
         //Input
         if input.update(&event) {
             let (new_act_slot, new_mode) = do_input(
-                &device, &queue, &input,
+                &wgpu_app.device, &wgpu_app.queue, &input,
                 &mut game, &mut slots, act_slot, mode,
                 &mut ui_state,
             );
@@ -365,8 +164,9 @@ async fn main() {
             mode = new_mode;
         }
 
-        let uniforms = Uniforms { translation, rotation, _padding: [0.0; 3],};
-        queue.write_buffer(uniform_buffer, 0, bytemuck::cast_slice(&[uniforms]));
+        let uniforms = Uniforms { translation: [0.0, 0.0, 0.0, 0.0], 
+            rotation: [0.0, 0.0, 0.0, 0.0], _padding: [0.0; 3],};
+        wgpu_app.queue.write_buffer(&wgpu_app.uniform_buffer, 0, bytemuck::cast_slice(&[uniforms]));
 
         //Render
 
@@ -396,8 +196,9 @@ async fn main() {
                 transparent_models.push(&game.cursor);
 
                 render(
-                    &surface, &device, &queue, &render_pipeline, &transparent_pipeline,
-                    bind_group, &buffers.depth_buffer.view,
+                    &surface, &wgpu_app.device, &wgpu_app.queue, &wgpu_app.render_pipeline, 
+                    &wgpu_app.transparent_pipeline,
+                    &wgpu_app.bind_group, &wgpu_app.depth_buffer.view,
                     &mut egui_manager,
                     &window,
                     |ctx| ui_state.render(ctx),
@@ -411,11 +212,11 @@ async fn main() {
                 window_id,
             } if window_id == window.id() => {
                 // Обновляем существующую конфигурацию
-                config.width = new_size.width;
-                config.height = new_size.height;
-                surface.configure(&device, &config);
+                wgpu_app.config.width = new_size.width;
+                wgpu_app.config.height = new_size.height;
+                surface.configure(&wgpu_app.device, &wgpu_app.config);
                 
-                buffers.depth_buffer.resize(&device, new_size);
+                wgpu_app.depth_buffer.resize(&wgpu_app.device, new_size);
 
                 // Запрашиваем перерисовку
                 window.request_redraw();
